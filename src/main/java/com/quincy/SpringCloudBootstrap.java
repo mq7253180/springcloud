379e0bce4d2088f173caa19579e906bd13dca0ed
Qiang Ma <mq7253180@126.com>
2019-11-28 14:31:02 +0800

    xxx

--- src/main/java/com/quincy/SpringCloudBootstrap.java
+++ src/main/java/com/quincy/SpringCloudBootstrap.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-//import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-//@EnableHystrixDashboard
+@EnableHystrixDashboard
 @EnableConfigServer
 @EnableZuulProxy
 @EnableEurekaServer
